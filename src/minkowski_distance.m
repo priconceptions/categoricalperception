f59e82414dc51051f0a84021a30a61c14e3ed822
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2018-03-20 21:14:26 -0400

    Update minkowski_distance.m

--- src/minkowski_distance.m
+++ src/minkowski_distance.m
@@ -1,5 +1,5 @@
 function [ dist ] = minkowski_distance(v1, v2, r)
-%EUCLIDEAN_DISTANCE: computes minkowski distance of two vectors
+%MINKOWSKI_DISTANCE: computes minkowski distance of two vectors
 %   Detailed explanation goes here
 dist  = (sum(abs(v1 - v2) .^ r)) ^ (1/r);
 end
